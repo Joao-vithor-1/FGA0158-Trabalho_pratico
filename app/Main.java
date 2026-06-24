75561b6cb2d1b31551dbf4d966590a962a52b9e3
Felipe Sant'Ana <felipelimastn@gmail.com>
2026-06-24 17:10:52 -0300

    feat(app): criar estrutura base e metodos vazios da classe Main

---
+++ app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.util.ArrayList;
+import Modelo.Cliente;
+import Modelo.Cliente_standard;
+import Modelo.Cliente_vip;
+import Modelo.Produto;
+import Modelo.Comida;
+
+@SuppressWarnings("unused")
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("=== Cafeteria Byte & Brew ===");
+		
+		cadastrarProdutosIniciais();
+		cadastrarClientesIniciais();
+		
+	}
+	
+	private static void cadastrarProdutosIniciais() {
+		System.out.println("[Sistema] Carregando cardápio de produtos...");
+	}
+	private static void cadastrarClientesIniciais() {
+		System.out.println("[Sistema] Carregando banco de clientes...");
+	}
+
+}
