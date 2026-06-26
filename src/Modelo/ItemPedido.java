3ac17e35fe9335dd808654b09d1235669195ae0f
Pichau <joaopvp7778@gmail.com>
2026-06-26 19:19:26 -0300

    Adiconado Classe Lista_Cliente com os sequintes instancias e metodos:
    private ArrayList<Cliente> lista
    public Lista_cliente()
    public boolean buscarCliente(String cpf)
    private int BuscarCliente(String cpf) não tenho um nome melhor
    public void removerCliente(Cliente cliente)
    public void removerCliente(String cpf)
    public boolean buscaClienteVip(String cpf)
    
    mudanças ItemPedido
    private void imprimirComida()
    private void imprimirBebida()
    public void ImprimirCardapio()

--- src/Modelo/ItemPedido.java
+++ src/Modelo/ItemPedido.java
@@ -2,6 +2,7 @@ package Modelo;
 
 import java.util.ArrayList;
 
+
 public class ItemPedido {
 
     protected ArrayList<Produto> lista;
@@ -17,11 +18,38 @@ public class ItemPedido {
     public void removerProduto(Produto produto) {
         lista.remove(produto);
     }
-
+    //porque tem um metodo get pra lista?
     public ArrayList<Produto> getLista() {
         return lista;
     }
-
+    
+    private void imprimirComida() {
+    		System.out.println("COMIDA : PRECO");
+    		for(int i = 0;i<lista.size();i++) {
+    		if(lista.get(i) instanceof Comida) {
+    			System.out.println(lista.get(i).getNomeProduto() +" :  R$" + lista.get(i).getPrecoBase());
+    		}
+    	}
+    }
+    private void imprimirBebida() {
+    	System.out.println("BEBIDA :  PRECO");
+		for(int i = 0;i<lista.size();i++) {
+		if(lista.get(i) instanceof Bebida) {
+			System.out.println(lista.get(i).getNomeProduto() +" :  R$" + lista.get(i).getPrecoBase());
+		}
+      }
+		
+    }
+    
+    public void ImprimirCardapio() {
+    	System.out.println();
+    	imprimirBebida();
+    	System.out.println();
+    	imprimirComida();
+    }
+    
+    //o nome pode confudir com a quantidade de comida e bebida total
+    //tipo se tenho bebida 1 com qtd 5 e comida 2 com qtd 7 achei que retornava 12
     public int quantidadeProdutos () {
         return lista.size();
     }
---
+++ src/Modelo/Lista_cliente.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Lista_cliente {
+	private ArrayList<Cliente> lista;
+	public Lista_cliente() {
+		lista = new ArrayList<>();
+	}
+	public void adicionarCliente(Cliente cliente) {
+		lista.add(cliente);
+	}
+	//obs pode usar nome tambem ja que ambos sao string
+	public boolean buscarCliente(String cpf) {
+		for(int i = 0;i<lista.size();i++) {
+			if(cpf.equals(lista.get(i).getCpf())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	//so pra retorna o index
+	private int BuscarCliente(String cpf) {
+		for(int i = 0;i<lista.size();i++) {
+			if(cpf.equals(lista.get(i).getCpf())) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void removerCliente(Cliente cliente) {
+		lista.remove(cliente);
+	}
+	// sobrecarga de metodo
+	public void removerCliente(String cpf) {
+		int remover = BuscarCliente(cpf);
+		if(remover!=-1) {
+			lista.remove(remover);
+		}
+	}
+	
+	public boolean buscaClienteVip(String cpf) {
+		int Buscar = BuscarCliente(cpf);
+		if(Buscar!=-1) {
+		return(lista.get(Buscar) instanceof Cliente_vip);
+		}
+		
+		return false;
+	}
+	
+}
