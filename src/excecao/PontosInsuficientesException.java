de047e1bb45906ac7eaca88fc07d171c4ba46b48
Pedro Magno <magnopteles@gmail.com>
2026-06-27 03:22:30 -0300

    adiciona PontosInsuficientesException

---
+++ src/excecao/PontosInsuficientesException.java
@@ -0,0 +1,21 @@
+package excecao;
+
+public class PontosInsuficientesException extends Exception {
+	
+	private final float pontosNecessarios;
+	private final float pontosDisponiveis;
+	
+	public PontosInsuficientesException(float pontosNecessarios, float pontosDisponiveis) {
+		super("Pontos de XP insuficientes: necessario " + pontosNecessarios + ", disponivel " + pontosDisponiveis);
+		this.pontosNecessarios = pontosNecessarios;
+		this.pontosDisponiveis = pontosDisponiveis;
+	}
+
+	public float getPontosNecessarios() {
+		return pontosNecessarios;
+	}
+	
+	public float getPontosDisponiveis() {
+		return pontosDisponiveis;
+	}
+}
