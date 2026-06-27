e0be101804d03de87a9ae2c3f4aa40fc420074aa
Paulo Eduardo <pauloeduardofimartins@gmail.com>
2026-06-27 00:16:53 -0300

    adiciona package excecao com EstoqueInsuficienteException

---
+++ src/excecao/EstoqueInsuficienteException.java
@@ -0,0 +1,29 @@
+package excecao;
+
+public class EstoqueInsuficienteException extends Exception {
+      
+    private final String nomeProduto;
+    private final int quantidadeSolicitada;
+    private final int quantidadeDisponivel;
+
+    public EstoqueInsuficienteException(String nomeProduto, int quantidadeSolicitada, int quantidadeDisponivel) {
+       
+        super("Estoque insuficiente para " + nomeProduto + ". Quantidade solicitada: " + quantidadeSolicitada + ", Quantidade disponível: " + quantidadeDisponivel);
+        this.nomeProduto = nomeProduto;
+        this.quantidadeSolicitada = quantidadeSolicitada;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public int getQuantidadeSolicitada() {
+        return quantidadeSolicitada;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+}
