c4de18b504c2a7ee21fe1d1b8f48af7d96fe817e
Pichau <joaopvp7778@gmail.com>
2026-06-27 10:14:12 -0300

    adicionei as classes CpfInvalidoException no pacote execao, e a classe VerificadorCpf, coloquei temporariamente no pacote modelo, nao sei em qual pacote colocar essa classe

---
+++ src/Modelo/VerificadorCpf.java
@@ -0,0 +1,20 @@
+package Modelo;
+import excecao.CpfInvalidoException;
+
+public  class VerificadorCpf{
+	
+	public static String verficadorCpf(String cpf) throws CpfInvalidoException{
+		String cpfVerificado = "";
+		for(int i =0;i<cpf.length();i++) {
+			char temp_char = cpf.charAt(i);
+			if(Character.isDigit(temp_char)) {
+				cpfVerificado+=temp_char;
+			}
+		}
+		if(cpfVerificado.length()!=11) {
+			throw new CpfInvalidoException(cpfVerificado,"cpf invalido");
+		}
+		return cpfVerificado;
+	}
+	
+}
---
+++ src/excecao/CpfInvalidoException.java
@@ -0,0 +1,12 @@
+package excecao;
+
+public class CpfInvalidoException extends Exception{
+	private final String cpf;
+	public CpfInvalidoException(String cpf, String erro) {
+				super(erro);
+				this.cpf = cpf;
+			}
+	public String getCpf() {
+		return cpf;
+	}
+}
